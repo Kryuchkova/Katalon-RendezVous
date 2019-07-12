b98b14173cad68d0daa464c8096038fede0bc740
Alexandra <skripka23@mail.ru>
2019-07-12 17:13:48 +0400

    The Reports for test suits of Login and Favorites have been update.

--- Test Suites/Favorites Test Suite.ts
+++ Test Suites/Favorites Test Suite.ts
@@ -11,15 +11,15 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>7a7d1a0a-d660-4096-a5c5-24815e50cec2</testSuiteGuid>
    <testCaseLink>
-      <guid>38d37227-96fb-4fd1-94fe-0705ac841a21</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Add product to favorites section for authorized user</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
       <guid>b5dc844e-43d8-423a-a2e8-e931e1033d19</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Add product to favorites section for unauthorized user</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>38d37227-96fb-4fd1-94fe-0705ac841a21</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Add product to favorites section for authorized user</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
